e8c3856e64e182911907f0e9c12fa8af0af4c02e
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-08 21:00:00 +0000

    modified:   character_types.rb character_types 100%

--- character_types.rb
+++ character_types.rb
@@ -14,3 +14,14 @@
 #  "Number of digits in the string is: 4"
 
 p "Enter a sentence:"
+sentence = gets.chomp
+
+letters = sentence.gsub(/[^a-z]/i, "")
+letter_count = letters.length
+
+spaces = sentence.count(" ")
+digits = sentence.gsub(/[^0-9]/,"")
+digits_count = digits.length
+p "Number of letters in the string is: " + letter_count.to_s + ""
+p "Number of spaces in the string is: " + spaces.to_s + ""
+p "Number of digits in the string is: " + digits_count.to_s + ""
