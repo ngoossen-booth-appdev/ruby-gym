8097c8df630775da00765e5eaa70e2aab18f368c
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-08 19:41:04 +0000

    modified:   count_the.rb count_the done 100%

--- count_the.rb
+++ count_the.rb
@@ -4,3 +4,13 @@
 #   and finally prints, "'the' appeared x times", where x is an Integer
 
 p "Enter a sentence:"
+user_sentence = gets.chomp.downcase
+user_sentence = user_sentence.gsub(/[^a-z0-9\s]/i, "")
+p user_sentence
+words = user_sentence.split(" ")
+
+the_count = words.count("the")
+
+
+
+p "'the' appeared " + the_count.to_s + " times."
