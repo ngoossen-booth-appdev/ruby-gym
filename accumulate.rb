ff17ffbe41ea2e1017603500a69ddeb387359d3b
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-10 15:08:17 +0000

    modified:   accumulate.rb accumulate 100%

--- accumulate.rb
+++ accumulate.rb
@@ -2,13 +2,20 @@
 #  prints the history of answers given while being pestered whether we're there yet.
 
 # Note: all the recorded answers should be lowercase
+history=Array.new
 
 p "Are we there yet?"
 
 answer = gets.chomp.downcase
 
+history.push(answer)
+
 while answer != "yes"
   p "Are we there yet?"
   
   answer = gets.chomp.downcase
+  
+  history.push(answer)
+
 end
+p history
