ad15412349040799063023e1f5baa2b16f000937
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-21 23:23:24 +0000

    modified:   anagram.rb anagram 100%

--- anagram.rb
+++ anagram.rb
@@ -6,5 +6,24 @@
 
 p "Enter two words separated by a comma"
 
-user_words = gets.chomp
+user_words = gets.chomp.split(",")
+
+word1_letters=user_words[0].downcase.gsub(" ","").strip.split("").sort
+word2_letters=user_words[1].downcase.gsub(" ","").strip.split("").sort
+
+# p word1_letters
+# p word2_letters
+
+# p word1_letters==word2_letters
+
+if word1_letters.length==word2_letters.length && word1_letters==word2_letters
+  p "true"
+else
+  p "false"
+end
+
+
+
+
+
 
